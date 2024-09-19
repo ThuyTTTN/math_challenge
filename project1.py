7aed460eae76f709726f1b3a51df0e479067d523
Thuy Nguyen <ttruong06@gmail.com>
2024-09-19 13:35:15 -0400

    guess the answer

--- project1.py
+++ project1.py
@@ -3,6 +3,7 @@ import random
 OPERATORS  = ["+", "-", "*"]
 MIN_OPERAND = 3
 MAX_OPERAND = 12    
+TOTAL_PROBLEMS = 10
 
 def generate_problem():
     left = random.randint(MIN_OPERAND, MAX_OPERAND)
@@ -15,5 +16,12 @@ def generate_problem():
     answer = eval(expression)
     return expression, answer
 
-expression, answer = generate_problem()
-print(expression, answer)
+for i in range(TOTAL_PROBLEMS):
+    expression, answer = generate_problem()
+    while True:
+        guess = input("Problem #" + str(i + 1) + ": " + expression + " = ")
+        if guess == str(answer):
+            break
+
+
+#print(expression, answer)
